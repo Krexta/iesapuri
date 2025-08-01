a1369442a305943b384360134edaaf2581e743f6
Krexta <sakuradev521@gmail.com>
2025-08-01 17:33:26 +0900

    色々追加など

---
+++ front/src/app/.storybook/main.ts
@@ -0,0 +1,44 @@
+// @ts-nocheck
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  core: {
+    builder: {
+      name: '@storybook/builder-vite',
+      options: {
+        viteConfigPath: './vite_storybook.config.ts',
+      },
+    },
+  },
+  stories: [
+    '../src/**/*.story.mdx',
+    '../src/**/*.story.@(js|jsx|ts|tsx)',
+    // '../../modules/src/**/*.story.mdx',
+    // '../../modules/src/**/*.story.@(js|jsx|ts|tsx)',
+    // '../../admin/src/**/*.story.mdx',
+    // '../../admin/src/**/*.story.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    'storybook-addon-react-router-v6',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
+    '@storybook/addon-interactions',
+    '@storybook/addon-designs',
+  ],
+  features: {
+    interactionsDebugger: true,
+  },
+  typescript: {
+    reactDocgen: 'react-docgen', // 👈 react-docgen configured here.
+  },
+  docs: {
+    autodocs: true,
+  },
+}
+
+export default config
---
+++ front/src/app/src/routes/login.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Login } from '@app/template/Login/Login'
+
+const Pages: React.FC = () => {
+  return <Login />
+}
+
+export default Pages
